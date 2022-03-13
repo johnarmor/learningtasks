2c9ee6c266c403ec9e7cabe293b04ef87f99c92e
johnarmor <100022397+johnarmor@users.noreply.github.com>
2022-03-13 22:23:05 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <title> My Home Page </title>
+    </head>
+    <body>
+    <h1> Welcome To My Website! I am Armor Espinosa!</h1>
+    <ul>
+       <li><a href="index.php">HOME</a></li>
+       <li><a href="/LT2/Recipe.html">LEARNING TASK 2</a></li>
+       <li><a href="/LT2/songs.html">LEARNING TASK 3</a></li>
+    </ul>
+ </body>
+</html>
