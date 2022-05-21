3c5d2614dd3ec9cc1df06ae78085ff0e523a031f
johnarmor <100022397+johnarmor@users.noreply.github.com>
2022-05-21 23:46:19 +0800

    Update index.php

--- LT6/index.php
+++ LT6/index.php
@@ -167,15 +167,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
     <!-- Core theme JS-->
     <script src="js/scripts.js"></script>
-
-
-
-
-
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript" src="js/custome.js"></script>
-    <script type="text/javascript" src="js/particles.min.js"></script>
-    <script type="text/javascript" src="js/app.js"></script>
     <script type="text/javascript" src="js/scripts.js"></script>
 
 
